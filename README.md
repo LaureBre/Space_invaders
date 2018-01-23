ff03f00880b84f4770922495b39f7e75eb63e72f
LaureBre <acs.laure.eole@gmail.com>
2018-01-23 16:58:30 +0100

    Starting project : index.html

---
+++ README.md
@@ -0,0 +1,2 @@
+# Space_invaders
+by Simon Arsenic, Samuel Teyzier &amp; Laure Eole
---
+++ js/space.js
@@ -0,0 +1,4 @@
+var obj = { propriete_1:   valeur_1,   // propriete_# peut être un identifiant
+            2:             valeur_2,   // ou un nombre
+            // ...,
+            "propriete n": valeur_n }; // ou une chaîne
