26a8bbe371a2a4bc71a64b8b45449b957225c9ba
LaureBre <acs.laure.eole@gmail.com>
2018-01-24 12:21:04 +0100

    Added 2 font icons with font css, Added box .conteneur, Started .js

--- js/space.js
+++ js/space.js
@@ -1,4 +1,48 @@
-var obj = { propriete_1:   valeur_1,   // propriete_# peut être un identifiant
-            2:             valeur_2,   // ou un nombre
-            // ...,
-            "propriete n": valeur_n }; // ou une chaîne
+///////////////// Déclaration des VARIABLES //////////////////////
+
+var decompte = 0;
+
+
+
+//////////////////// Déclaration des OBJETS ///////////////////////
+
+
+///// Le constructeur !
+
+var Entite = function(dom) {
+      this.dom = dom;
+      this.alive = true;
+      this.hit = false;
+      this.nbLife = 1;
+      this.speed = 1
+      this.hurt = function(){
+        this.nbLife--;
+        function() {
+          this.dom.class = "icon-explode"
+        }
+        if (this.nbLife == 0) {
+          this.alive = false;
+          this.dead = function(){
+            setTimeout(mafonction, 1500);
+
+          }
+        };
+        return (this.nbLife);
+      }
+
+    };
+
+///// Nos objets
+
+var ennemis = new Entite {
+      posX: number,
+      posY: number,
+      speed: number
+    };
+
+var player = new Entite {
+      posX: 50
+    };
+///////////////////////// FONCTIONS /////////////////////////
+
+Entite.prototype
